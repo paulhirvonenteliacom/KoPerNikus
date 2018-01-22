48c8ff08954f133ac757db7af6b79183866e04c0
Paul Hirvonen <paul.hirvonen@telia.com>
2018-01-22 18:39:30 +0100

    Layout improvment

--- Sjuklöner/Migrations/201801221729112_initial.Designer.cs
+++ Sjuklöner/Migrations/201801221729112_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Sjuklöner.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class init : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201801221605017_init"; }
+            get { return "201801221729112_initial"; }
         }
         
         string IMigrationMetadata.Source
--- Sjuklöner/Migrations/201801221729112_initial.cs
+++ Sjuklöner/Migrations/201801221729112_initial.cs
@@ -3,7 +3,7 @@ namespace Sjuklöner.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class init : DbMigration
+    public partial class initial : DbMigration
     {
         public override void Up()
         {
