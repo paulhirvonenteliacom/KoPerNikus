0e53d98af1f9af7ab3eba20d70975ef27dde8b98
Paul Hirvonen <paul.hirvonen@telia.com>
2018-02-23 14:30:10 +0100

    Added new migration.

---
+++ Sjuklöner/Migrations/201802231329426_addRequiredToAssistantClass.cs
@@ -0,0 +1,32 @@
+namespace Sjuklöner.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addRequiredToAssistantClass : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Assistants", "FirstName", c => c.String(nullable: false));
+            AlterColumn("dbo.Assistants", "LastName", c => c.String(nullable: false));
+            AlterColumn("dbo.Assistants", "AssistantSSN", c => c.String(nullable: false));
+            AlterColumn("dbo.Assistants", "Email", c => c.String(nullable: false));
+            AlterColumn("dbo.Assistants", "PhoneNumber", c => c.String(nullable: false));
+            AlterColumn("dbo.Assistants", "HolidayPayRate", c => c.String(nullable: false));
+            AlterColumn("dbo.Assistants", "PayrollTaxRate", c => c.String(nullable: false));
+            AlterColumn("dbo.Assistants", "PensionAndInsuranceRate", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Assistants", "PensionAndInsuranceRate", c => c.String());
+            AlterColumn("dbo.Assistants", "PayrollTaxRate", c => c.String());
+            AlterColumn("dbo.Assistants", "HolidayPayRate", c => c.String());
+            AlterColumn("dbo.Assistants", "PhoneNumber", c => c.String());
+            AlterColumn("dbo.Assistants", "Email", c => c.String());
+            AlterColumn("dbo.Assistants", "AssistantSSN", c => c.String());
+            AlterColumn("dbo.Assistants", "LastName", c => c.String());
+            AlterColumn("dbo.Assistants", "FirstName", c => c.String());
+        }
+    }
+}
