426ff7c6fa5542c4ed9a5f5671aeebf0056a6012
envh <e.niklashagman@gmail.com>
2018-02-23 16:03:36 +0100

    Added regex to OmbudEditVM

--- Sjuklöner/Viewmodels/OmbudEditVM.cs
+++ Sjuklöner/Viewmodels/OmbudEditVM.cs
@@ -27,6 +27,7 @@ namespace Sjuklöner.Viewmodels
 
         [Required]
         [Display(Name = "Personnummer")]
+        [RegularExpression(@"(((20)((0[0-9])|(1[0-7])))|(([1][^0-8])?\d{2}))((0[1-9])|1[0-2])((0[1-9])|(1[0-9])|(2[0-9])|(3[01]))?\d{4}$")]
         public string SSN { get; set; }
 
         [Required]
