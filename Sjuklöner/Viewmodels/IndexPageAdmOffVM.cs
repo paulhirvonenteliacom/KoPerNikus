9b80edad8f34fe5c42485da3d3b1016e2dfee724
Paul Hirvonen <paul.hirvonen@telia.com>
2018-07-30 15:52:31 +0200

    Improved filter for claims in index pages for ombud, admoff and admin.

--- Sjuklöner/Viewmodels/IndexPageAdmOffVM.cs
+++ Sjuklöner/Viewmodels/IndexPageAdmOffVM.cs
@@ -3,11 +3,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Sjuklöner.Viewmodels
 {
     public class IndexPageAdmOffVM
     {
+        public int NumberOfDecided { get; set; }
+
+        public int NumberOfInbox { get; set; }
+
+        public int NumberOfReview { get; set; }
+
+        public int NumberOfDecidedFiltered { get; set; }
+
+        public int NumberOfInboxFiltered { get; set; }
+
+        public int NumberOfReviewFiltered { get; set; }
+
+        public bool MyClaims { get; set; }
+
+        public int? SelectedTimePeriodId { get; set; }
+
+        public IEnumerable<SelectListItem> TimePeriods { get; set; }
+
+        public int? SelectedKeyId { get; set; }
+
+        public IEnumerable<SelectListItem> Keys { get; set; }
+
+        public string SearchString { get; set; }
+
+        public string FilterTextDecided { get; set; }
+
+        public string FilterTextInbox { get; set; }
+
+        public string FilterTextReview { get; set; }
+
         public List<Claim> DecidedClaims  { get; set; }  //Beslutade, old "Avslagna"
 
         public List<Claim> InInboxClaims { get; set; }  //I inkorgen
--- Sjuklöner/Viewmodels/IndexPageOmbudVM.cs
+++ Sjuklöner/Viewmodels/IndexPageOmbudVM.cs
@@ -3,11 +3,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Sjuklöner.Viewmodels
 {
     public class IndexPageOmbudVM
     {
+        public int NumberOfDecided { get; set; }
+
+        public int NumberOfDraft { get; set; }
+
+        public int NumberOfReview { get; set; }
+
+        public int NumberOfDecidedFiltered { get; set; }
+
+        public int NumberOfDraftFiltered { get; set; }
+
+        public int NumberOfReviewFiltered { get; set; }
+
+        public bool MyClaims { get; set; }
+
+        public int? SelectedTimePeriodId { get; set; }
+
+        public IEnumerable<SelectListItem> TimePeriods { get; set; }
+
+        public int? SelectedKeyId { get; set; }
+
+        public IEnumerable<SelectListItem> Keys { get; set; }
+
+        public string SearchString { get; set; }
+
+        public string FilterTextDecided { get; set; }
+
+        public string FilterTextDraft { get; set; }
+
+        public string FilterTextReview { get; set; }
+
         public bool AssistantsExist { get; set; }
 
         public string CompanyName { get; set; }
